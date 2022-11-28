70d8a44eb8553e2d2d79007c9db8fb22b2670ada
Carolina Belen Maio <cmaio@MacBook-Air-de-Carolina.local>
2022-11-27 23:46:03 -0300

    [Add] Interface

--- src/api/ITaTeTiTDA.java
+++ src/api/ITaTeTiTDA.java
@@ -1,8 +1,14 @@
 package api;
 
 public interface ITaTeTiTDA {
+    boolean validatePosition(int pos);
+    boolean boardCompleted();
     void Inicializar();
     void Turno() throws Exception;
     void Turno(int pos) throws Exception;
     void Jugar(int posicion) throws Exception;
+    int getResult();
+    void JugarPc();
+    void mostrarTablero(int p);
+
 }
